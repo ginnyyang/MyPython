7d974873e26a0ef345bc2ae1385901f43be46499
yangjing <yangjing@jingoal.com>
2018-07-12 11:59:08 +0800

    update enroll.py

--- enroll.py
+++ enroll.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def enroll(name,gender):
+#如果要继续传入年龄、城市等信息，我们可以把年龄和城市设为默认参数
+def enroll(name,gender,age=6,city='Beijing'):
 	print('name:',name)
 	print('gender:',gender)
+	print('age:',age)
+	print('city:',city)
 	
 enroll('Sarah','F')
+#只有与默认参数不符的学生才需要提供额外的信息
+enroll('Bob','M',7)
+enroll('Adam','M',city='Tianjin')
