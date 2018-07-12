bfa99d8e0a84f83faacdc957d050b905d7b9d6db
yangjing <yangjing@jingoal.com>
2018-07-12 15:57:17 +0800

    add prictice0712.py

---
+++ prictice0712.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def product(*args):
+	s=1
+	for i in args:
+		s=s*i
+	return s
+	
+print('product(5)=',product(5))
+print('product(5, 6) =', product(5, 6))
+print('product(5, 6, 7) =', product(5, 6, 7))
+print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
+if product(5)!=5:
+	print('test failed')
+elif product(5, 6) != 30:
+    print('测试失败!')
+elif product(5, 6, 7) != 210:
+    print('测试失败!')
+elif product(5, 6, 7, 9) != 1890:
+    print('测试失败!')
+else:
+    try:
+        product()
+        print('测试失败!')
+    except TypeError:
+        print('测试成功!')
