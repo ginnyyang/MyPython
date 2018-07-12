7fc1bdbe3b43209a9ec8135950b487d0d804ae83
yangjing <yangjing@jingoal.com>
2018-07-12 15:06:46 +0800

    update person.py

--- person.py
+++ person.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#关键字参数允许你传入0个或任意个含参数名的参数，这些关键字参数在函数内部自动组装为一个dict
+#和可变参数类似，也可以先组装出一个dict，然后，把该dict转换为关键字参数传进去
 
 def person(name,age,**kw):
 	print('name:',name,'age:',age,'other:',kw)
 
-person('Michael',30)
-person('Bob',35,city='Beijing')
-person('Adam',45,gender='M',job='Engineer')
+extra={'city':'Beijing','job':'Engineer'}
+person('Jack',24,city=extra['city'],job=extra['job'])
+#上面复杂的调用可以简化
+#**extra表示把extra这个dict的所有key-value用关键字参数传入到函数**kw
+person('Jack',24,**extra)
