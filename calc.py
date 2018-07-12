cf29385e265adb04529f337b5d5f763807ec526a
yangjing <yangjing@jingoal.com>
2018-07-12 14:32:29 +0800

    update calc.py

--- calc.py
+++ calc.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#如果参数个数不固定，可以把参数作为一个list或tuple传进来
-def calc(numbers):
+#利用可变参数，调用函数的方式可简化
+def calc(*numbers):#*代表可变参数
 	sum=0
 	for n in numbers:
 		sum = sum + n * n 
 	return sum
 	
-print(calc([1,2,3]))
-print(calc((1,3,5,7)))
-#利用可变参数，调用函数的方式可简化，目前是报错的
 print(calc(1,2,3))
 print(calc(1,3,5,7))
+print(calc(1,2))
+print(calc())
