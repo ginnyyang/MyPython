03d6aca6e180d720166df224b1d7518cd28fbf7b
yangjing <yangjing@jingoal.com>
2018-07-20 11:40:11 +0800

    update decorator.py

--- learning_process/decorator.py
+++ learning_process/decorator.py
@@ -22,19 +22,24 @@
 
 #因为返回的那个wrapper()函数名字就是'wrapper'，所以，需要把原始函数的__name__等属性复制到wrapper()函数中，否则，有些依赖函数签名的代码执行就会出错。
 #不需要编写wrapper.__name__ = func.__name__这样的代码，Python内置的functools.wraps就是干这个事的，所以，一个完整的decorator的写法如下
+##针对带参数的decorator
 import functools
 
-def log(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kw):
-        print('call %s():' % func.__name__)
-        return func(*args, **kw)
-    return wrapper
+def log(text):
+	def decorator(func):
+		@functools.wraps(func)
+		def wrapper(*agrs,**kw):
+			print('%s %s():'%(text,func.__name__))
+			return func(*agrs,**kw)
+		return wrapper
+	return decorator
 
-@log
+@log('execute')
 def now():
 	print('2018-7-20')
 now()
 print(now.__name__)
+
+
 
 
