13a6aad9768968c1aebc11ab9152570d8286498c
yangjing <yangjing@jingoal.com>
2018-07-20 11:33:03 +0800

    update decorator.py

--- learning_process/decorator.py
+++ learning_process/decorator.py
@@ -1,29 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#假设我们要增强now()函数的功能，比如，在函数调用前后自动打印日志，但又不希望修改now()函数的定义，
-#这种在代码运行期间动态增加功能的方式，称之为“装饰器”（Decorator）
-#本质上，decorator就是一个返回函数的高阶函数
-#我们要定义一个能打印日志的decorator，可以定义如下
-#def log(func):
-#	def wrapper(*agrs,**kw):
-#		print('call %s():'%func.__name__)
-#		return func(*agrs,**kw)
-#	return wrapper
-#观察上面的log，因为它是一个decorator，所以接受一个函数作为参数，并返回一个函数。
-#我们要借助Python的@语法，把decorator置于函数的定义处：
-#把@log放到now()函数的定义处，相当于执行了语句：
-#now = log(now)
-#由于log()是一个decorator，返回一个函数，
-#所以，原来的now()函数仍然存在，只是现在同名的now变量指向了新的函数，于是调用now()将执行新函数，
-#即在log()函数中返回的wrapper()函数。
-#wrapper()函数的参数定义是(*args, **kw)，因此，wrapper()函数可以接受任意参数的调用。
-#在wrapper()函数内，首先打印日志，再紧接着调用原始函数。
-#@log
-#def now():
-#	print('2018-07-19')
-#now()
-
 #如果decorator本身需要传入参数，那就需要编写一个返回decorator的高阶函数，写出来会更复杂。比如，要自定义log的文本
 def log(text):
 	def decorator(func):
@@ -36,5 +13,11 @@ def log(text):
 def now():
 	print('2018-07-20')
 now()
+
+#我们来剖析上面的语句，首先执行log('execute')，返回的是decorator函数，
+#再调用返回的函数，参数是now函数，返回值最终是wrapper函数。
+#以上两种decorator的定义都没有问题，但还差最后一步。
+#因为我们讲了函数也是对象，它有__name__等属性，但你去看经过decorator装饰之后的函数，它们的__name__已经从原来的'now'变成了'wrapper'
+print(now.__name__)
 
 
