3df1df4da0c6f54d1ffddbf2a984943b9778a894
yangjing <yangjing@jingoal.com>
2018-07-20 16:06:40 +0800

    update decorator_practice0720.py

--- learning_process/decorator_practice0720.py
+++ learning_process/decorator_practice0720.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#请编写一个decorator，能在函数调用的前后打印出'begin call'和'end call'的日志
+#再思考一下能否写出一个@log的decorator，使它既支持：
+#@log
+#def f():
+#    pass
+#又支持：
+#
+#@log('execute')
+#def f():
+#    pass
+
 import functools
 import time
 
-def metric(fn):
-    @functools.wraps(fn)
-    def wrapper(*args, **kwargs):
-        print('begin call------')
-        before = time.time()
-        r=fn(*args, **kwargs)
-        consumed=(time.time()-before)*1000
-        print('%s executed in %s ms' % (fn.__name__, consumed))
-        print('end call-----------')
-        return r
-    return wrapper
+def log(text=''):
+    def metric(fn):
+        @functools.wraps(fn)
+        def wrapper(*args, **kwargs):
+            print('%s begin call %s'%(text,fn.__name__))
+            before = time.time()
+            r=fn(*args, **kwargs)
+            consumed=(time.time()-before)*1000
+            print('%s executed in %s ms' % (fn.__name__, consumed))
+            print('%s end call %s'%(text,fn.__name__))
+            return r
+        return wrapper
+    return metric
 
-@metric
+@log()
 def fast(x,y):
     time.sleep(0.0012)
     return x + y
 print(fast(11,22))
+
+@log('execute')
+def slow(x, y, z):
+    time.sleep(0.1234)
+    return x * y * z
+print(slow(1,2,3))
 
 
 
