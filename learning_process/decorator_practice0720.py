e2586b7b31ad87048aad549dec353d25e32f830e
yangjing <yangjing@jingoal.com>
2018-07-20 15:45:10 +0800

    update decorator_practice.py

--- learning_process/decorator_practice0720.py
+++ learning_process/decorator_practice0720.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#设计一个decorator，它可作用于任何函数上，并打印该函数的执行时间
+#请编写一个decorator，能在函数调用的前后打印出'begin call'和'end call'的日志
 import functools
 import time
 
 def metric(fn):
     @functools.wraps(fn)
     def wrapper(*args, **kwargs):
+        print('begin call------')
         before = time.time()
         r=fn(*args, **kwargs)
         consumed=(time.time()-before)*1000
         print('%s executed in %s ms' % (fn.__name__, consumed))
+        print('end call-----------')
         return r
     return wrapper
 
@@ -19,6 +21,7 @@ def fast(x,y):
     time.sleep(0.0012)
     return x + y
 print(fast(11,22))
+
 
 
 
