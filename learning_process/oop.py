8a7d727d522521b4e555234f6ee7c168e88c701a
yangjing <yangjing@jingoal.com>
2018-07-23 17:44:28 +0800

    update oop.py

--- learning_process/oop.py
+++ learning_process/oop.py
@@ -5,9 +5,7 @@
 #OOP把对象作为程序的基本单元，一个对象包含了数据和操作数据的函数。
 #面向对象的设计思想是抽象出Class，根据Class创建Instance
 
-#如果要让内部属性不被外部访问，可以把属性的名称前加上两个下划线__，
-#在Python中，实例的变量名如果以__开头，就变成了一个私有变量（private），只有内部可以访问，外部不能访问，
-#所以，我们把Student类改一改：
+#如果外部代码要获取name和score怎么办？可以给Student类增加get_name和get_score这样的方法：
 class Student(object):
 
 	def __init__(self,name,score):
@@ -16,6 +14,12 @@ class Student(object):
 	
 	def print_score(self):
 		print('%s:%s'%(self.__name,self.__score))
+		
+	def get_name(self):
+		return self.__name
+		
+	def get_score(self):
+		return self.__score
 		
 #	def get_grade(self):
 #		if self.score>90:
@@ -26,4 +30,5 @@ class Student(object):
 #			return 'C'
 	
 yangjing=Student('Ginny Yang',100)
-print(yangjing.__name)
+print(yangjing.get_name(),':',yangjing.get_score())
+
