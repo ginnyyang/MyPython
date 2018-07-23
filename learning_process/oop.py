92eee6d1bb7dfe08076cf2b4077865d27ef4e994
yangjing <yangjing@jingoal.com>
2018-07-23 16:45:02 +0800

    update oop.py

--- learning_process/oop.py
+++ learning_process/oop.py
@@ -5,13 +5,16 @@
 #OOP把对象作为程序的基本单元，一个对象包含了数据和操作数据的函数。
 #面向对象的设计思想是抽象出Class，根据Class创建Instance
 
-#在Python中，定义类是通过class关键字
-#class后面紧接着是类名，即Student，类名通常是大写开头的单词，
-#紧接着是(object)，表示该类是从哪个类继承下来的,通常，如果没有合适的继承类，就使用object类，这是所有类最终都会继承的类。
+#由于类可以起到模板的作用，因此，可以在创建实例的时候，把一些我们认为必须绑定的属性强制填写进去。
+#通过定义一个特殊的__init__方法，在创建实例的时候，就把name，score等属性绑上去
 class Student(object):
-	pass
+
+	def __init__(self,name,score):
+		self.name=name
+		self.score=score
 	
-#可以自由地给一个实例变量绑定属性
-yangjing=Student()
-yangjing.name='Ginny Yang'
+#注意到__init__方法的第一个参数永远是self，表示创建的实例本身，因此，在__init__方法内部，就可以把各种属性绑定到self，因为self就指向创建的实例本身
+#有了__init__方法，在创建实例的时候，就不能传入空的参数了，必须传入与__init__方法匹配的参数，但self不需要传，Python解释器自己会把实例变量传进去
+yangjing=Student('Ginny Yang',100)
 print(yangjing.name)
+print(yangjing.score)
