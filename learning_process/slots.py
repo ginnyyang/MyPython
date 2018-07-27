95c09ddc3e32d81eaedaefbdafeb47d018100202
yangjing <yangjing@jingoal.com>
2018-07-27 12:01:02 +0800

    update slots.py

--- learning_process/slots.py
+++ learning_process/slots.py
@@ -1,37 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #正常情况下，当我们定义了一个class，创建了一个class的实例后，我们可以给该实例绑定任何属性和方法，这就是动态语言的灵活性。
+#如果我们想要限制实例的属性怎么办？比如，只允许对Student实例添加name和age属性
+#为了达到限制的目的，Python允许在定义class的时候，定义一个特殊的__slots__变量，来限制该class实例能添加的属性
 
 class Student(object):
-	pass
+	__slots__=('name','age')
 	
-#创建一个实例
+
 s=Student()
-#实例绑定一个属性
 s.name='yangjing'
 print(s.name)
-
-
-#实例绑定一个方法
-def set_age(self,age):
-	self.age=age
-	
-from types import MethodType
-s.set_age=MethodType(set_age,s)
-s.set_age(25)
+s.age=25
 print(s.age)
+#s.score=99
+#print(s.score)#由于'score'没有被放到__slots__中，所以不能绑定score属性，试图绑定score将得到AttributeError的错误
 
-#但是，给一个实例绑定的方法，对另一个实例是不起作用的
-s2=Student()
-#s2.set_age(30)
-#print(s2.age)#AttributeError: 'Student' object has no attribute 'set_age'
+#使用__slots__要注意，__slots__定义的属性仅对当前类实例起作用，对继承的子类是不起作用的
+class GraduateStudent(Student):
+	pass
 
-#为了给所有实例都绑定方法，可以给class绑定方法
-def set_score(self,score):
-	self.score=score
-	
-Student.set_score=set_score
-s.set_score(90)
-s2.set_score(100)
-print(s.score)
-print(s2.score)
+g=GraduateStudent()
+g.score=100
+print(g.score)
+
+#除非在子类中也定义__slots__，这样，子类实例允许定义的属性就是自身的__slots__加上父类的__slots__
