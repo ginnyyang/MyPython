aa82cee358117476955f298ab1b8e0a1644d21e8
yangjing <yangjing@jingoal.com>
2018-07-28 15:33:18 +0800

    update property.py

--- learning_process/property.py
+++ learning_process/property.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#为了限制score的范围，可以通过一个set_score()方法来设置成绩，再通过一个get_score()来获取成绩，这样，在set_score()方法里，就可以检查参数
+#有没有既能检查参数，又可以用类似属性这样简单的方式来访问类的变量呢？
+#对于类的方法，装饰器一样起作用。Python内置的@property装饰器就是负责把一个方法变成属性调用的：
+
+#@property的实现比较复杂，我们先考察如何使用。把一个getter方法变成属性，只需要加上@property就可以了，
+#此时，@property本身又创建了另一个装饰器@score.setter，负责把一个setter方法变成属性赋值，于是，我们就拥有一个可控的属性操作
 
 class Student(object):
 	
-	def get_score(self):
+	@property
+	def score(self):
 		return self._score
-		
-	def set_score(self,value):
+	
+	@score.setter
+	def score(self,value):
 		if not isinstance(value,int):
 			raise ValueError('score must be an integer')
 		if value <0 or value>100:
@@ -16,7 +22,7 @@ class Student(object):
 		
 #现在，对任意的Student实例进行操作，就不能随心所欲地设置score了
 s=Student()
-s.set_score(60)
-print(s.get_score())
+s.score=60
+print(s.score)
 #s.set_score('asd')#ValueError: score must be an integer
-#s.set_score(999)#ValueError: score must be 0~100
+s.score=999#ValueError: score must be 0~100
