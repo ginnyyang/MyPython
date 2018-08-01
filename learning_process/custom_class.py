78198c261d072764de0f5ad3441f62c428953144
yangjing <yangjing@jingoal.com>
2018-08-01 11:12:39 +0800

    update custom_class.py

--- learning_process/custom_class.py
+++ learning_process/custom_class.py
@@ -4,42 +4,22 @@
 #__slots__我们已经知道怎么用了，参见slots.py ,__len__()方法我们也知道是为了能让class作用于len()函数。
 #除此之外，Python的class中还有许多这样有特殊用途的函数，可以帮助我们定制类。
 
-#__str__
-#只需要定义好__str__()方法，返回一个好看的字符串就可以了：
-class Student(object):
-	def __init__(self,name):
-		self.name=name
+#__iter__
+#如果一个类想被用于for ... in循环，类似list或tuple那样，就必须实现一个__iter__()方法，该方法返回一个迭代对象，
+#然后，Python的for循环就会不断调用该迭代对象的__next__()方法拿到循环的下一个值，直到遇到StopIteration错误时退出循环。
+#以斐波那契数列为例，写一个Fib类，可以作用于for循环：
+class Fib(object):
+	def __init__(self):
+		self.a,self.b=0,1
 		
-	def __str__(self):
-		return 'Student object (name:%s)'%self.name
+	def __iter__(self):
+		return self
+		
+	def __next__(self):
+		self.a,self.b=self.b,self.a+self.b
+		if self.a>100000:
+			raise StopIteration()
+		return self.a
 
-print(Student('yangjing'))
-#Student object (name:yangjing)
-#这样打印出来的实例，不但好看，而且容易看出实例内部重要的数据。
-#但是细心一点会发现直接敲变量不用print，打印出来的实例还是不好看
-#这个需要在交互模式下尝试
-#>>> class Student(object):
-#...     def __init__(self, name):
-#...             self.name=name
-#...     def __str__(self):
-#...             return 'Student object (name:%s)'%self.name
-#...
-#>>> s=Student('yangjing')
-#>>> s
-#<__main__.Student object at 0x000001F0209B6278>
-
-#这是因为直接显示变量调用的不是__str__()，而是__repr__()，
-#两者的区别是__str__()返回用户看到的字符串，而__repr__()返回程序开发者看到的字符串，
-#也就是说，__repr__()是为调试服务的。
-#解决办法是再定义一个__repr__()。但是通常__str__()和__repr__()代码都是一样的，所以，有个偷懒的写法：
-
-#>>> class Student(object):
-#...     def __init__(self,name):
-#...             self.name=name
-#...     def __str__(self):
-#...             return 'Student object (name:%s)'%self.name
-#...     __repr__ = __str__
-#...
-#>>> s=Student('yangjing')
-#>>> s
-#Student object (name:yangjing)
+for n in Fib():
+	print(n)
