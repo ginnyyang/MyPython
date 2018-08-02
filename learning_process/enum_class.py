f286cfafe857aaea2959b5091b3c73ebebb8e1c6
yangjing <yangjing@jingoal.com>
2018-08-02 16:33:30 +0800

    add enum_class.py

---
+++ learning_process/enum_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#当我们需要定义常量时,更好的方法是为这样的枚举类型定义一个class类型，然后，每个常量都是class的一个唯一实例。Python提供了Enum类来实现这个功能：
+from enum import Enum
+
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+
+for name, member in Month.__members__.items():
+    print(name, '=>', member, ',', member.value)#value属性则是自动赋给成员的int常量，默认从1开始计数
