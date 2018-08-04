0394232b5b5ae69c3d3c193b967c47148dd88015
yangjing <yangjing@jingoal.com>
2018-08-04 17:48:52 +0800

    update debug.py

--- learning_process/debug.py
+++ learning_process/debug.py
@@ -1,39 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#第4种方式是启动Python的调试器pdb，让程序以单步方式运行，可以随时查看运行状态
+#第5种方式也是用pdb，但是不需要单步执行，我们只需要import pdb，然后，在可能出错的地方放一个pdb.set_trace()，就可以设置一个断点：
+import pdb
+
 s='0'
 n=int(s)
+pdb.set_trace()
 print(10/n)
 
 #执行结果：
-#python -m pdb debug.py------->启动
-#> c:\users\jingoal\mypython\learning_process\debug.py(5)<module>()
-#-> s='0'--------------------->pdb定位到下一步要执行的代码-> s = '0'
-#(Pdb) l---------------------->输入命令l来查看代码
-#  1     #!/usr/bin/env python3
-#  2     # -*- coding: utf-8 -*-
-#  3
-#  4     #第4种方式是启动Python的调试器pdb，让程序以单步方式运行，可以随时查看运行状态
-#  5  -> s='0'
-#  6     n=int(s)
-#  7     print(10/n)
-#  8
-#  9     #执行结果：
-# 10
-# 11
-#(Pdb) n------------------------>输入命令n可以单步执行代码
-#> c:\users\jingoal\mypython\learning_process\debug.py(6)<module>()
-#-> n=int(s)
-#(Pdb) p s--------------------->任何时候都可以输入命令p 变量名来查看变量
-#'0'
-#(Pdb) n
-#> c:\users\jingoal\mypython\learning_process\debug.py(7)<module>()
+#运行代码，程序会自动在pdb.set_trace()暂停并进入pdb调试环境，可以用命令p查看变量，或者用命令c继续运行
+#python debug.py
+#> c:\users\jingoal\mypython\learning_process\debug.py(10)<module>()
 #-> print(10/n)
 #(Pdb) p n
 #0
-#(Pdb) q---------------------->输入命令q结束调试，退出程序
+#(Pdb) c
+#Traceback (most recent call last):
+#  File "debug.py", line 10, in <module>
+#    print(10/n)
+#ZeroDivisionError: division by zero
 
-#这种通过pdb在命令行调试的方法理论上是万能的，但实在是太麻烦了，如果有一千行代码，要运行到第999行得敲多少命令啊
+#这个方式比直接启动pdb单步调试效率要高很多，但也高不到哪去。
+
+#如果要比较爽地设置断点、单步执行，就需要一个支持调试功能的IDE。目前比较好的Python IDE有：
+#Visual Studio Code：https://code.visualstudio.com/，需要安装Python插件。
+#PyCharm：http://www.jetbrains.com/pycharm/
+#另外，Eclipse加上pydev插件也可以调试Python程序。
+#写程序最痛苦的事情莫过于调试，程序往往会以你意想不到的流程来运行，你期待执行的语句其实根本没有执行，这时候，就需要调试了。
+#虽然用IDE调试起来比较方便，但是最后你会发现，logging才是终极武器
 
 
