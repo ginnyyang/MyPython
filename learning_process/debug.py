8a309c67cb5f63bc9def8e5728acbb604b821956
yangjing <yangjing@jingoal.com>
2018-08-04 17:10:52 +0800

    update debug.py

--- learning_process/debug.py
+++ learning_process/debug.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#第一种方法简单直接粗暴有效，就是用print()把可能有问题的变量打印出来看看：
+#第二种方法凡是用print()来辅助查看的地方，都可以用断言（assert）来替代：
 def foo(s):
 	n=int(s)
-	print('>>>n=%d'%n)#用print()最大的坏处是将来还得删掉它，想想程序里到处都是print()，运行结果也会包含很多垃圾信息。
+	assert n!=0,'n is zero'#assert的意思是，表达式n != 0应该是True，否则，根据程序运行的逻辑，后面的代码肯定会出错。如果断言失败，assert语句本身就会抛出AssertionError：
 	return 10/n
 	
 def main():
@@ -13,7 +13,17 @@ def main():
 main()
 
 #执行结果：
-#>>>n=0
+#Traceback (most recent call last):
+#  File "debug.py", line 13, in <module>
+#    main()
+#  File "debug.py", line 11, in main
+#    foo('0')
+#  File "debug.py", line 7, in foo
+#    assert n!=0,'n is zero'
+#AssertionError: n is zero
+
+#程序中如果到处充斥着assert，和print()相比也好不到哪去。不过，启动Python解释器时可以用-O参数来关闭assert：
+#python -O debug.py
 #Traceback (most recent call last):
 #  File "debug.py", line 13, in <module>
 #    main()
@@ -22,5 +32,3 @@ main()
 #  File "debug.py", line 8, in foo
 #    return 10/n
 #ZeroDivisionError: division by zero
-
-#
