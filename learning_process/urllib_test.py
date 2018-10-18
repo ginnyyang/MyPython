5c1aab5440fbc59f0c1dbdf0422dc6e193bfd9fc
yangjing <yangjing@jingoal.com>
2018-10-18 15:23:25 +0800

    update urllib_test.py

--- learning_process/urllib_test.py
+++ learning_process/urllib_test.py
@@ -1,29 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#如果要以POST发送一个请求，只需要把参数data以bytes形式传入。
-#我们模拟一个微博登录，先读取登录的邮箱和口令，然后按照weibo.cn的登录页的格式以username=xxx&password=xxx的编码传入：
-from urllib import request,parse
+#利用urllib读取JSON，然后将JSON解析为Python对象
+from urllib import request
+import json
 
-print('Login to weibo.cn...')
-email=input('Email:')
-passwd=input('Password:')
-login_data=parse.urlencode([
-	('username',email),
-	('password',passwd),
-	('entry','mweibo'),
-	('cliend_id',''),
-	('savestate','1'),
-	('ec','')
-])
-
-req=request.Request('https://passport.weibo.cn/sso/login')
-req.add_header('Origin', 'https://passport.weibo.cn')
-req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
-req.add_header('Referer','https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=http%3A%2F%2Fm.weibo.cn%2F')
-
-with request.urlopen(req,data=login_data.encode('utf-8')) as f:
-	print('Status:',f.status,f.reason)
-	for k,v in f.getheaders():
-		print('%s:%s'%(k,v))
-	print('Data:',f.read().decode('utf-8'))
+def fetch_data(url):
+	with request.urlopen(url) as f:
+		data=f.read()
+		return json.loads(data.decode('utf-8'))
+# 测试
+URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=json'
+data = fetch_data(URL)
+print(data)
+assert data['query']['results']['channel']['location']['city'] == 'Beijing'
+print('ok')
