c8831eaf6719edd40a675eaeb3acb9abed17955e
yangjing <yangjing@jingoal.com>
2018-10-18 15:14:29 +0800

    update urllib_test.py

--- learning_process/urllib_test.py
+++ learning_process/urllib_test.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#如果我们要想模拟浏览器发送GET请求，就需要使用Request对象，通过往Request对象添加HTTP头，我们就可以把请求伪装成浏览器。
-#例如，模拟iPhone 6去请求豆瓣首页：
-from urllib import request
+#如果要以POST发送一个请求，只需要把参数data以bytes形式传入。
+#我们模拟一个微博登录，先读取登录的邮箱和口令，然后按照weibo.cn的登录页的格式以username=xxx&password=xxx的编码传入：
+from urllib import request,parse
 
-req=request.Request('http://www.douban.com/')
+print('Login to weibo.cn...')
+email=input('Email:')
+passwd=input('Password:')
+login_data=parse.urlencode([
+	('username',email),
+	('password',passwd),
+	('entry','mweibo'),
+	('cliend_id',''),
+	('savestate','1'),
+	('ec','')
+])
+
+req=request.Request('https://passport.weibo.cn/sso/login')
+req.add_header('Origin', 'https://passport.weibo.cn')
 req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+req.add_header('Referer','https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=http%3A%2F%2Fm.weibo.cn%2F')
 
-with request.urlopen(req) as f:
+with request.urlopen(req,data=login_data.encode('utf-8')) as f:
 	print('Status:',f.status,f.reason)
 	for k,v in f.getheaders():
 		print('%s:%s'%(k,v))
