e9cd8878fe12f0e1830c804b2f625ce1ae342886
yangjing <yangjing@jingoal.com>
2018-10-18 14:48:06 +0800

    add urllib_test.py

---
+++ learning_process/urllib_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#urllib的request模块可以非常方便地抓取URL内容，也就是发送一个GET请求到指定的页面，然后返回HTTP的响应
+#例如，对豆瓣的一个URLhttps://api.douban.com/v2/book/2129650进行抓取，并返回响应：
+from urllib import request
+
+with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
+	data=f.read()
+	print('Status:',f.status,f.reason)
+	for k,v in f.getheaders():
+		print('%s:%s'%(k,v))
+	print('Data:',data.decode('utf-8'))
