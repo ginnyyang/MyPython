71cc72612291d335ed59c9983aa8c7ef26110abc
yangjing <yangjing@jingoal.com>
2018-12-05 16:25:47 +0800

    update xml_test.py

--- learning_process/xml_test.py
+++ learning_process/xml_test.py
@@ -13,28 +13,36 @@
 #char_data事件，在读取python时；
 #end_element事件，在读取</a>时。
 
+#请利用SAX编写程序解析Yahoo的XML格式的天气预报，获取天气预报：
+#https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=xml
+
+#参数woeid是城市代码，要查询某个城市代码，可以在weather.yahoo.com搜索城市，浏览器地址栏的URL就包含城市代码。
+
 from xml.parsers.expat import ParserCreate
+from urllib import request
 
-class DefaultSaxHandler(object):
-	def start_element(self,name,attrs):
-		print('sax:start_element:%s,attrs:%s'%(name,str(attrs)))
-		
-	def end_element(self,name):
-		print('sax:end_element:%s'%name)
-		
-	def char_data(self,text):
-		print('sax:char_data:%s'%text)
-		
-xml = r'''<?xml version="1.0"?>
-<ol>
-	<li><a href="/python">Python</a></li>
-	<li><a href="/ruby">Ruby</a></li>
-</ol>
-'''
+class MySaxHandler(object):
+     def __init__(self):
+         self.result = dict(city=None, forecast=[])
+     def start_element(self, name, attr):
+         if attr.get("city"):
+             self.result["city"] = attr["city"]
+         if attr.get("date") and not attr.get("temp"):
+             self.result["forecast"].append(dict(date=attr["date"], high=attr["high"],low=attr["low"]))
 
-handler=DefaultSaxHandler()
-parser=ParserCreate()
-parser.StartElementHandler=handler.start_element
-parser.EndElementHandler=handler.end_element
-parser.CharacterDataHandler=handler.char_data
-parser.Parse(xml)
+def parseXml(xml_str):
+     parser = ParserCreate()
+     handler = MySaxHandler()
+     parser.StartElementHandler = handler.start_element
+     parser.Parse(xml_str)
+     return handler.result
+
+# 测试:
+URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=xml'
+
+with request.urlopen(URL, timeout=4) as f:
+	data = f.read()
+
+result = parseXml(data.decode('utf-8'))
+print (result)
+assert result['city'] == 'Beijing'
