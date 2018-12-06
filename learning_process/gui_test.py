dc45a15be228f9361f71bf95bb117988aa9009ff
yangjing <yangjing@jingoal.com>
2018-12-06 19:58:50 +0800

    update gui_test.py

--- learning_process/gui_test.py
+++ learning_process/gui_test.py
@@ -3,6 +3,7 @@
 #在GUI中，每个Button、Label、输入框等，都是一个Widget。
 #Frame则是可以容纳其他Widget的Widget，所有的Widget组合起来就是一棵树
 #pack()方法把Widget加入到父容器中，并实现布局。pack()是最简单的布局，grid()可以实现更复杂的布局
+#再对这个GUI程序改进一下，加入一个文本框，让用户可以输入文本，然后点按钮后，弹出消息对话框
 
 from tkinter import *
 import tkinter.messagebox as messagebox
@@ -14,10 +15,14 @@ class Application(Frame):
 		self.createWidgets()
 		
 	def createWidgets(self):
-		self.helloLabel=Label(self,text='Hello,world!')
-		self.helloLabel.pack()
-		self.quitButton=Button(self,text='Quit',command=self.quit)
-		self.quitButton.pack()
+		self.nameInput=Entry(self)
+		self.nameInput.pack()
+		self.alertButton=Button(self,text='Hello',command=self.hello)
+		self.alertButton.pack()
+	
+	def hello(self):
+		name=self.nameInput.get() or 'world'
+		messagebox.showinfo('Message','Hello,%s'%name)
 		
 app=Application()
 app.master.title('Hello World')
