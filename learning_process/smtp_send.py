a0b38f6340560fbbd8c6cca5f196ba3dfefbc8d6
yangjing <yangjing@jingoal.com>
2019-03-12 14:56:29 +0800

    update smtp_send4 by send image in html

--- learning_process/smtp_send.py
+++ learning_process/smtp_send.py
@@ -53,8 +53,15 @@ msg['From'] = _format_addr('Python爱好者 <%s>' % from_addr)
 msg['To'] = _format_addr('管理员 <%s>' % to_addr)
 msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
 
+#如果要把一个图片嵌入到邮件正文中怎么做？
+#直接在HTML邮件中链接图片地址行不行？答案是，大部分邮件服务商都会自动屏蔽带有外链的图片，因为不知道这些链接是否指向恶意网站。
+#要把图片嵌入到邮件正文中，我们只需按照发送附件的方式，先把邮件作为附件添加进去，然后，在HTML中通过引用src="cid:0"就可以把附件作为图片嵌入了。
+#如果有多个图片，给它们依次编号，然后引用不同的cid:x即可
 #邮件正文是MIMEText:
-msg.attach(MIMEText('send with file...', 'plain', 'utf-8'))
+msg.attach(MIMEText('<html><body><h1>Hello</h1>'+
+	'<p><img src="cid:0"></p>'+
+	'</body></html>', 'html', 'utf-8'))
+
 #添加附件就是加上一个MIMEBase，从本地读取一个图片:
 with open('C:/Users/jingoal/MyPython/learning_process/2523reopen0510.png','rb') as f:
 	#设置附件的MIME和文件名，这里是png类型:
