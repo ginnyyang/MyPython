f872824f89eda43758d945a833104d6ebc55ef07
yangjing <yangjing@jingoal.com>
2019-03-20 11:17:34 +0800

    update app.py and add three htmls

--- learning_process/app.py
+++ learning_process/app.py
@@ -8,29 +8,38 @@
 #注意噢，同一个URL/signin分别有GET和POST两种请求，映射到两个处理函数中
 
 #Flask通过Python的装饰器在内部自动地把URL和函数给关联起来
+#用MVC模式改写一下
 from flask import Flask
 from flask import request
+from flask import render_template
 
 app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def home():
-	return '<h1>Home</h1>'
+	#return '<h1>Home</h1>'
+	return render_template('home.html')
 	
 @app.route('/signin',methods=['GET'])
 def signin_form():
-	return '''<form action="/signin" method="post">
-              <p><input name="username"></p>
-              <p><input name="password" type="password"></p>
-              <p><button type="submit">Sign In</button></p>
-              </form>'''
+	#return '''<form action="/signin" method="post">
+    #          <p><input name="username"></p>
+    #          <p><input name="password" type="password"></p>
+    #          <p><button type="submit">Sign In</button></p>
+    #          </form>'''
+	return render_template('form.html')
 			  
 @app.route('/signin',methods=['POST'])
 def signin():
 	# 需要从request对象读取表单内容：
-	if request.form['username']=='admin' and request.form['password']=='password':
-		return '<h3>Hello,admin!</h3>'
-	return '<h3>Bad username or password.</h3>'
+	#if request.form['username']=='admin' and request.form['password']=='password':
+	#	return '<h3>Hello,admin!</h3>'
+	#return '<h3>from flask import</h3>'
+	username = request.form['username']
+	password = request.form['password']
+	if username=='admin' and password =='password':
+		return render_template('signin-ok.html',username=username)
+	return render_template('form.html',message='Bad username or password',username=username)
 	
 if __name__=='__main__':
 	app.run()
